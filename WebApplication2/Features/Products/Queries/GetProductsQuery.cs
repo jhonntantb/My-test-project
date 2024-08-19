35c7ba2c11029cdc83f9d1e0c019fdf3eb7f728e
jhonntantb <jhonntantb@gmail.com>
2024-08-19 15:34:06 -0300

    Add Product query

---
+++ WebApplication2/Features/Products/Queries/GetProductsQuery.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Infrastructure.Persistence;
+
+namespace WebApplication2.Features.Products.Queries
+{
+    public class GetProductsQuery : IRequest<List<GetProductsQueryResponse>>
+    {
+    }
+
+    public class GetProductsQueryHandler : IRequestHandler<GetProductsQuery, List<GetProductsQueryResponse>>
+    {
+        private readonly MyAppDbContext _context;
+        public GetProductsQueryHandler(MyAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<List<GetProductsQueryResponse>> Handle(GetProductsQuery request, CancellationToken cancellationToken) 
+        {
+            return _context.Products
+                .AsNoTracking()
+                .Select(x => new GetProductsQueryResponse
+                {
+                    ProductId = x.ProductId,
+                    Description = x.Description,
+                    Price = x.Price,
+                })
+                .ToListAsync();
+        }
+    }
+    public class GetProductsQueryResponse
+    {
+        public int ProductId { get; set; }
+        public string Description { get; set; } = default!;
+        public double Price { get; set; }
+    }
+}
