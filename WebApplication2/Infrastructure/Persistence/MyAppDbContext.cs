16c66a23cf7a962343a4da28ff4f1c357f463658
jhonntantb <jhonntantb@gmail.com>
2024-08-19 15:03:17 -0300

    First commit

---
+++ WebApplication2/Infrastructure/Persistence/MyAppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Domain;
+
+namespace WebApplication2.Infrastructure.Persistence
+{
+    public class MyAppDbContext : DbContext
+    {
+        public MyAppDbContext(DbContextOptions<MyAppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
